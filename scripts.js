7b239f4b807db61e983d6d5964dd35b5e17d2a59
Fabio-Pinho <fabiopinhodev@gmail.com>
2025-04-10 10:24:23 -0300

    responsividade da pagina

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let imagem = document.querySelector(".starbucks")
+let circulo = document.querySelector(".circulo")
+
+function trocaimagem(endereco){
+    imagem.src = endereco
+}
+function trocaCor(cor){
+    circulo.style.background = cor
+    }
